b0846efc6d6ecf512a39573e378c1b7ead57b342
jselbaz <jselbaz@gmail.com>
2016-03-25 12:08:58 -0500

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,13 @@
+# Declare second integer, double, and String variables.
+i2 = raw_input()
+d2 = raw_input()
+s2 = raw_input()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print int(i) + int(i2)
+# Print the sum of the double variables on a new line.
+print float(d) + float(d2)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s + s2
